5bc1e949adbee55e9e5e047b2531337b9e285398
joselfv <joselfv.1979@gmail.com>
2024-02-22 08:33:46 +0100

    Add getRoleService

--- books-node-api/src/services/userService.ts
+++ books-node-api/src/services/userService.ts
@@ -1,3 +1,4 @@
+import Role, { IRole } from "../models/Role";
 import User, { IUser } from "../models/User";
 import { ObjectId } from "mongodb";
 
@@ -8,11 +9,11 @@ export async function getUsersService() {
 }
 
 export async function getUserService(id: string) {
-  return await User.findById(new ObjectId(id));
+  return await User.findById(new ObjectId(id)).populate('roles', 'name');
 }
 
 export async function getUsernameService(username: string) {
-  return await User.findOne({ username: username });
+  return await User.findOne({ username: username }).populate('roles', 'name');
 }
 
 export async function getEmailService(email: string) {
@@ -24,9 +25,28 @@ export async function createUserService(user: IUser) {
 }
 
 export async function updateUserService(id: string, user: IUser) {
-  return await User.findByIdAndUpdate(new ObjectId(id), user, { new: true });
+  return await User.findByIdAndUpdate(new ObjectId(id), user, { new: true }).populate('roles', 'name');
 }
 
 export async function deleteUserService(id: string) {
   return await User.findByIdAndDelete(new ObjectId(id));
 }
+
+export async function getRoleService(roles: string[]) {
+
+  let roleList: IRole[] = [];
+
+  for (let role of roles) {
+    const auxRole = await Role.findOne({ name: role });
+    if (auxRole) roleList.push(auxRole);
+  }
+
+  if (roleList.length === 0) {
+    const userRole = await Role.findOne({ name: 'USER' });
+    if (userRole) roleList.push(userRole);
+  }
+
+  return roleList;
+}
+
+
