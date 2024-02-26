f744b3a372d3912cdea277581410cf23a55efc95
joselfv <joselfv.1979@gmail.com>
2024-02-26 06:46:37 +0100

    Import new store

--- books-react-app/src/index.tsx
+++ books-react-app/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { DeleteModalContextProvider } from './context/deleteModal/DeleteModalContextProvider';
+import { store } from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <React.StrictMode>
