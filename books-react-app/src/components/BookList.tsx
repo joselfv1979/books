91756427c5461cff3858a4e3d04d514b591a39b3
joselfv <joselfv.1979@gmail.com>
2024-03-05 06:34:08 +0100

    Refactor BookList

--- books-react-app/src/components/BookList.tsx
+++ books-react-app/src/components/BookList.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import BookCard from './BookCard';
 import { Row, Col } from 'react-bootstrap';
 import styles from '../assets/scss/BookList.module.scss';
@@ -7,22 +6,24 @@ import { Book } from '../types/Book';
 
 const BookList = () => {
     const { books } = useAppSelector((state) => state.book);
-    const noBooks = books.length === 0;
+
     return (
-        <>
-            <h1>Books</h1>
-            <Row data-testid="book-list">
-                {books.length > 0 &&
-                    books.map((book: Book) => {
+        books.length > 0 ?
+            <div className='p-3'>
+                <h1>Books</h1>
+                <Row>
+                    {books.map((book: Book) => {
                         return (
                             <Col key={book.id} sm={4} data-testid="book-card">
                                 <BookCard book={book} styles={styles} />
                             </Col>
                         );
                     })}
-                {noBooks && <p style={{ textAlign: 'center' }}>No books found</p>}
-            </Row>
-        </>
+                </Row>
+            </div>
+            : <div className='h-100 d-flex justify-content-center align-items-center pb-5'>
+                <h2 className='mb-5 text-center text-dark'>No books found</h2>
+            </div>
     );
 };
 
