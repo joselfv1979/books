35d01603b9272c05ea5472174587287f4d91761c
joselfv <joselfv.1979@gmail.com>
2024-06-05 08:54:05 +0200

    Create BookSearchBar component

---
+++ books-react-app/src/components/BookSearchBar.tsx
@@ -0,0 +1,28 @@
+import { useAppDispatch } from 'hooks/redux-hooks';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+
+type Props = {
+    query: { search?: string; page: number; };
+    setQuery: Dispatch<SetStateAction<{ search?: string; page: number }>>
+}
+
+const BookSearchBar = ({ query, setQuery }: Props) => {
+    const { getBooks } = useAppDispatch();
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setQuery((prevState) => ({ ...prevState, search: event.target.value, page: 1 }))
+    };
+
+    const handleSearch = () => {
+        getBooks(query);
+    };
+
+    return (
+        <div className='d-flex w-50 mx-auto mt-4'>
+            <input type="text" className="form-control mx-2" placeholder='Search for title, author, genre, ISBN, publisher' value={query?.search ?? ''} onChange={handleInputChange} />
+            <button type="submit" className="btn btn-primary" onClick={handleSearch}>Search</button>
+        </div>
+    );
+};
+
+export default BookSearchBar;
