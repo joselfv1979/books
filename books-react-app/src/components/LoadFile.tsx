2e798fd628a7a640a45472127a9ba67de79baf92
joselfv <joselfv.1979@gmail.com>
2024-07-12 07:37:50 +0200

    Refactor DeleteModal & LoadFile

--- books-react-app/src/components/LoadFile.tsx
+++ books-react-app/src/components/LoadFile.tsx
@@ -1,43 +1,44 @@
-import { ChangeEvent, RefObject, useState } from 'react'
-import { Form } from 'react-bootstrap'
+import { ChangeEvent, RefObject, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import styles from '../assets/scss/loadFile.module.scss';
 
 type Props = {
-    image?: File, // Can receive a file 
-    imagePath: string; // Or a file path
+    image: string;
     fileInput: RefObject<HTMLInputElement>;
     handleFile: (event: ChangeEvent<HTMLInputElement>) => void // Form file handler
 }
 
 // Custom file loader component with loader and image preview
-const LoadFile = ({ image, imagePath, fileInput, handleFile }: Props) => {
+const LoadFile = ({ image, fileInput, handleFile }: Props) => {
 
-    const currentImagePath = `${process.env.REACT_APP_API_URL}/${imagePath}`;
-
-    const imageExist = image ?? imagePath;
-    const fileName = image ?? imagePath ? imagePath.replace("public\\", "") : "";
+    const imageUrl = `${process.env.REACT_APP_API_URL}/${image}`;
 
     // Preview state management
-    const [preview, setPreview] = useState<string>(currentImagePath);
+    const [preview, setPreview] = useState(image ? imageUrl : null);
 
     const handleImage = (event: ChangeEvent<HTMLInputElement>) => {
-        const target = event.target as HTMLInputElement;
-
-        if (target.files) {
+        if (event.target.files) {
+            displayImage(event.target.files[0])
             handleFile(event);
-            // Object to create the preview
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setPreview(reader.result as string);
-            };
-            reader.readAsDataURL(target.files[0]);
         }
     };
 
+    const displayImage = (file: File) => {
+        // Object to create the preview
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setPreview(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+    }
+
     return (
         <>
-            <label htmlFor='photo'>Photo</label>
+            <label
+                htmlFor='photo'
+                className={styles.photoLabel}>Photo</label>
             <Form.Control
+                id='photo'
                 name="image"
                 type="file"
                 style={{ display: 'none' }}
@@ -46,12 +47,11 @@ const LoadFile = ({ image, imagePath, fileInput, handleFile }: Props) => {
             />
 
             <div className={styles.photoContainer}>
-                {imageExist ?
+                {preview ?
                     <img src={preview} alt='preview' className={styles.photo} />
                     : <span className={styles.photoNoFileText}>No file selected</span>
                 }
             </div>
-            <span className={styles.photoText}>{fileName}</span>
         </>
     )
 }
