c97baa4bb405206eecb22254a3e695196e17ae16
joselfv <joselfv.1979@gmail.com>
2024-10-25 06:20:32 +0200

    Refactor database conncetion

--- books-node-api/src/config/config.ts
+++ books-node-api/src/config/config.ts
@@ -7,30 +7,39 @@ dotenv.config({
 });
 
 const {
-  PROD_PORT,
-  PROD_HOST,
   DEV_HOST,
   DEV_PORT,
   DEV_DATABASE,
+  PROD_PORT,
+  PROD_HOST,
   PROD_DATABASE,
+  TEST_HOST,
+  TEST_PORT,
   TEST_DATABASE,
   NODE_ENV
 } = process.env;
 
-const HOST = NODE_ENV === "development" ? DEV_HOST : PROD_HOST;
-const PORT = NODE_ENV === "development" ? DEV_PORT : PROD_PORT;
-
 let db = '';
+let host = '';
+let port = '';
 
 if (NODE_ENV === "development") {
   db = DEV_DATABASE as string;
+  host = DEV_HOST as string;
+  port = DEV_PORT as string
 } else if (NODE_ENV === "test") {
   db = TEST_DATABASE as string;
+  host = TEST_HOST as string;
+  port = TEST_PORT as string;
 } else {
   db = PROD_DATABASE as string;
+  host = PROD_HOST as string;
+  port = PROD_PORT as string;
 }
 
 const DB_CONNECTION = db;
+const HOST = host;
+const PORT = port;
 
 export { DB_CONNECTION, HOST, NODE_ENV, PORT };
 
